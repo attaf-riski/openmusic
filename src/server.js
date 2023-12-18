88c51dcb1e40e9caee1a063f26f56cc3567dd1a5
attaf-riski <atafriski27@gmail.com>
2023-12-18 18:38:34 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,76 @@
+/* eslint-disable max-len */
+require('dotenv').config();
+
+const ClientError = require('./exceptions/ClientError');
+const Hapi = require('@hapi/hapi');
+const AlbumsValidator = require('./validator/albums');
+const AlbumsService = require('./services/postgree/AlbumsService');
+const SongsValidator = require('./validator/songs');
+const SongsService = require('./services/postgree/SongsService');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+      validator: AlbumsValidator,
+    },
+  });
+
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
+    }});
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const {response} = request;
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue;
+      }
+      // penanganan server error sesuai kebutuhan
+      console.log(response);
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue;
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
